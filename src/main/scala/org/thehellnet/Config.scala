871a0e81c304720360af9408c399c08acde3ce21
Stefano Lande <landeste@gmail.com>
2021-12-13 20:07:15 +0100

    handle time functionally

--- src/main/scala/org/thehellnet/Config.scala
+++ src/main/scala/org/thehellnet/Config.scala
@@ -6,6 +6,6 @@ object Config {
   val AUDIO_PORT   = 1235
   val PACKET_SIZE  = 8192
 
-  val CLIENT_TTL              = 10 // seconds
+  val CLIENT_TTL              = 10L // seconds
   val CLIENT_EXPIRATION_CHECK = 1 // every x seconds
 }
--- src/main/scala/org/thehellnet/service/ClientRegistrationService.scala
+++ src/main/scala/org/thehellnet/service/ClientRegistrationService.scala
@@ -1,6 +1,6 @@
 package org.thehellnet.service
 
-import cats.effect.{IO, Ref}
+import cats.effect.{Clock, IO, Ref}
 import org.thehellnet.Config
 import org.thehellnet.model.RadioClient
 import org.thehellnet.model.valueclass.ClientUpdateTime
@@ -8,8 +8,8 @@ import org.thehellnet.network.RadioClientChannel
 import org.typelevel.log4cats.StructuredLogger
 import org.typelevel.log4cats.slf4j.Slf4jLogger
 
-import java.time.LocalDateTime
 import java.time.temporal.ChronoUnit
+import java.time.{Instant, LocalDateTime, ZoneOffset}
 import java.util.concurrent.TimeUnit
 import scala.concurrent.duration.FiniteDuration
 
@@ -19,16 +19,18 @@ class ClientRegistrationService(radioClientChannel: RadioClientChannel) {
 
   def receiveClient(clientsR: Ref[IO, Map[RadioClient, ClientUpdateTime]]): IO[Unit] =
     for {
-      client <- radioClientChannel.receive()
-      _      <- logger.info(s"received client $client")
-      _      <- clientsR.getAndUpdate(addOrUpdateClients(client, _))
-      _      <- receiveClient(clientsR)
+      client     <- radioClientChannel.receive()
+      _          <- logger.info(s"received client $client")
+      nowInstant <- Clock[IO].realTimeInstant
+      _          <- clientsR.getAndUpdate(addOrUpdateClients(client, _, nowInstant))
+      _          <- receiveClient(clientsR)
     } yield ()
 
   def expireClients(clientsR: Ref[IO, Map[RadioClient, ClientUpdateTime]]): IO[Unit] =
     for {
+      nowInstant <- Clock[IO].realTimeInstant
       activeClients <- clientsR.modify { clientsMap =>
-        val notExpiredClients = clientsMap.filter(isAlive)
+        val notExpiredClients = clientsMap.filter(isAlive(_, nowInstant))
         (notExpiredClients, clientsMap)
       }
       _ <- logger.info(s"active clients ${activeClients.keySet.mkString("[", ",", "]")}")
@@ -36,16 +38,17 @@ class ClientRegistrationService(radioClientChannel: RadioClientChannel) {
       _ <- expireClients(clientsR)
     } yield ()
 
-  private def isAlive(client: (RadioClient, ClientUpdateTime)): Boolean = client match {
+  private def isAlive(client: (RadioClient, ClientUpdateTime), nowInstant: Instant): Boolean = client match {
     case (_, receivedAt) =>
-      val now = LocalDateTime.now
+      val now = LocalDateTime.ofInstant(nowInstant, ZoneOffset.UTC)
       receivedAt.value.plus(Config.CLIENT_TTL, ChronoUnit.SECONDS).isAfter(now)
   }
 
   private def addOrUpdateClients(newClient: RadioClient,
-                                 clientsMap: Map[RadioClient, ClientUpdateTime]): Map[RadioClient, ClientUpdateTime] = {
+                                 clientsMap: Map[RadioClient, ClientUpdateTime],
+                                 nowInstant: Instant): Map[RadioClient, ClientUpdateTime] = {
 
-    val now = ClientUpdateTime(LocalDateTime.now)
+    val now = ClientUpdateTime(LocalDateTime.ofInstant(nowInstant, ZoneOffset.UTC))
     clientsMap.filterNot { case (client, _) => client == newClient } + (newClient -> now)
   }
 
